1fcaf9253d7640c31c3c400805c0a9ae71c0c843
Robert Turner <roboturnerdev@gmail.com>
2021-11-21 06:01:37 -0500

    flipping cells

--- src/Board.js
+++ src/Board.js
@@ -32,25 +32,27 @@ class Board extends Component {
   }
 
   flipCellsAround(coord) {
-    let {ncols, nrows} = this.props;
+    // console.log('flipping', coord);
+    let {nCols, nRows} = this.props;
     let board = this.state.board;
-    let [y, x] = coord.split("-").map(Number);
-
+    let [y, x] = coord.split("-").map(Number);  // splitting the coord into two numbers y and x
 
     function flipCell(y, x) {
-      // if this coord is actually on board, flip it
-
-      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+      if (x >= 0 && x < nCols && y >= 0 && y < nRows) {
         board[y][x] = !board[y][x];
       }
     }
+    // flip intial cell
+    flipCell(y, x);
+    // then flip the updownleftright
+    flipCell(y, x-1);
+    flipCell(y, x+1);
+    flipCell(y-1, x);
+    flipCell(y+1, x);
 
-    // TODO: flip this cell and the cells around it
-
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
-
-    // this.setState({board, hasWon});
+    // for now just to keep it working
+    let hasWon = false;
+    this.setState({board, hasWon});
   }
 
   render() {
@@ -58,10 +60,11 @@ class Board extends Component {
     for(let y = 0; y < this.props.nRows; y++ ) {
       let row = [];
       for(let x = 0; x < this.props.nCols; x++) {
-        let coord = `${y}-${x}`;  // set each cell to have its coord as the unique key
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>); // loops over the rows, then columns
-      }                                                   // and draws a cell passing down whether it is on or not as prop
-      tblBoard.push(<tr key={y}>{row}</tr>);    // push the whole row element
+        let coord = `${y}-${x}`; 
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} 
+          flipCellsAroundMe={() => this.flipCellsAround(coord)} />);  // bind and pass argument
+      }                                                  
+      tblBoard.push(<tr key={y}>{row}</tr>);
     }
     return (
       <table className="Board">
